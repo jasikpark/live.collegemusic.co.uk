1ecbc72196182d5d5dad08b145c557ca71171b84
Caleb Jasik <calebjasik@jasik.xyz>
2020-08-26 01:00:36 -0500

    Add artist profiles

--- .eleventy.js
+++ .eleventy.js
@@ -7,6 +7,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
 
+  eleventyConfig.addPassthroughCopy("static/");
+
   eleventyConfig.addShortcode("version", function () {
     return String(Date.now());
   });

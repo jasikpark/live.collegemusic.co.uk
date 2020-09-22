f80668d92f854a628ba204982a4a8a1b067953df
Caleb Jasik <calebjasik@jasik.xyz>
2020-09-21 20:32:41 -0500

    Add canonical url

--- site.js
+++ site.js
@@ -104,6 +104,19 @@ function initSongData() {
         self.songData = data;
       };
     },
+
+    shareSong: function () {
+      if (navigator.share) {
+        navigator
+          .share({
+            title: "web.dev",
+            text: "Check out web.dev.",
+            url: "https://web.dev/",
+          })
+          .then(() => console.log("Successful share"))
+          .catch((error) => console.log("Error sharing", error));
+      }
+    },
   };
 }
 

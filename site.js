8f34e540617282aad87df77843dd88999963b9de
Caleb Jasik <calebjasik@jasik.xyz>
2020-11-09 22:42:49 -0600

    Remove the extraneous "getWeather" function

--- site.js
+++ site.js
@@ -29,38 +29,6 @@ function initWeather() {
     weather_icon: "",
     weather_description: "",
     getWeather: function () {
-      const geoOptions = {
-        maximumAge: 5 * 60 * 1000,
-        timeout: 10 * 1000,
-      };
-      var self = this;
-
-      function success(position) {
-        const latitude = position.coords.latitude;
-        const longitude = position.coords.longitude;
-
-        fetch(
-          `https://api.openweathermap.org/data/2.5/weather?appid=3074e25313624bc7213df098d33cd414&lat=${latitude}&lon=${longitude}`
-        )
-          .then((response) => response.json())
-          .then((data) => {
-            self.weather_icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-          });
-      }
-
-      function error() {
-        console.error("Unable to retrieve your location");
-        alert("Allow location for the wee little weather icon plzz");
-      }
-
-      if (!navigator.geolocation) {
-        console.log("Geolocation is not supported by your browser");
-      } else {
-        console.log("Locating…");
-        navigator.geolocation.getCurrentPosition(success, error, geoOptions);
-      }
-    },
-    getWeather2: function () {
       var self = this;
       fetch("https://geolocation-db.com/json/")
         .then((response) => response.json())
@@ -70,7 +38,6 @@ function initWeather() {
           )
             .then((response) => response.json())
             .then((data) => {
-              console.log(data);
               self.weather_icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
               self.weather_description = data.weather[0].description;
             });

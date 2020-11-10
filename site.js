075c2d3295d1ef32251fe99afa64109e03c1f8cd
Caleb Jasik <calebjasik@jasik.xyz>
2020-11-10 00:23:35 -0600

    exploring adding fullscreen functionality

--- site.js
+++ site.js
@@ -193,3 +193,34 @@ function initArtistHero() {
     },
   };
 }
+
+function initFullscreen() {
+  return {
+    toggleFullscreen: function () {
+      var doc = window.document;
+      var docEl = doc.documentElement;
+
+      var requestFullScreen =
+        docEl.requestFullscreen ||
+        docEl.mozRequestFullScreen ||
+        docEl.webkitRequestFullScreen ||
+        docEl.msRequestFullscreen;
+      var cancelFullScreen =
+        doc.exitFullscreen ||
+        doc.mozCancelFullScreen ||
+        doc.webkitExitFullscreen ||
+        doc.msExitFullscreen;
+
+      if (
+        !doc.fullscreenElement &&
+        !doc.mozFullScreenElement &&
+        !doc.webkitFullscreenElement &&
+        !doc.msFullscreenElement
+      ) {
+        requestFullScreen.call(docEl);
+      } else {
+        cancelFullScreen.call(doc);
+      }
+    },
+  };
+}

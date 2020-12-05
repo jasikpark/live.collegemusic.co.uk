79c3db50f1c9a3ca0ab708d160f865661fab77fe
Caleb Jasik <calebjasik@jasik.xyz>
2020-12-05 13:20:30 -0600

    chore(deps): migrate from postcss-clean to cssnano due to postcss 8 incompatibility

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
       safelist: [],
     },
   },
+  darkMode: false,
   theme: {
     colors: {
       transparent: "transparent",
@@ -129,38 +130,15 @@ module.exports = {
       "gray-translucent": "rgba(18, 21, 96, 0.17)",
     }),
     extend: {
-      width: { 7: "1.75rem", 72: "18rem", 116: "29rem", 232: "58rem" },
-      height: { 7: "1.75rem" },
+      width: { 116: "29rem", 232: "58rem" },
       zIndex: { "-10": "-10" },
-      inset: { "-6": "-1.5rem", "-12": "-3rem" },
       padding: { 30: "7.5rem", "1/8": "12.5%", "1/4": "25%" },
     },
   },
   variants: {
-    textColor: [
-      "responsive",
-      "hover",
-      "focus",
-      "group-hover",
-      "group-focus",
-      "active",
-    ],
-    backgroundColor: [
-      "responsive",
-      "hover",
-      "focus",
-      "group-hover",
-      "group-focus",
-      "active",
-    ],
-    backgroundOpacity: [
-      "responsive",
-      "hover",
-      "focus",
-      "group-hover",
-      "group-focus",
-      "active",
-    ],
+    textColor: ["group-focus", "active"],
+    backgroundColor: ["group-focus", "active"],
+    backgroundOpacity: ["group-focus", "active"],
   },
   plugins: [],
 };

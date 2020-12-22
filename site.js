503c05fa7727e8a520cb807511cd174285623e0c
Caleb Jasik <calebjasik@jasik.xyz>
2020-12-22 14:25:37 -0600

    fix: use window.location.origin to pass the origin to youtube iframe

--- site.js
+++ site.js
@@ -440,7 +440,7 @@ function onYouTubeIframeAPIReady() {
     player: new YT.Player("player", {
       height: "390",
       width: "640",
-      target: document.querySelector("link[rel='canonical']").href,
+      target: window.location.origin,
       videoId: "MCkTebktHVc",
       playerVars: { playsinline: 1 },
       events: {

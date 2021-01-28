ea6eb8bac873dc7d79145b7830e728d8fe3a8227
Caleb Jasik <calebjasik@jasik.xyz>
2021-01-28 14:34:33 -0600

    fix: make the cards more transparent

--- styles/tailwind.config.js
+++ styles/tailwind.config.js
@@ -158,7 +158,7 @@ module.exports = {
         "song-fade": { easing: "ease-in", color: ["rgba(0,0,0,0)", "#000"] },
         "song-fade-white": {
           easing: "ease-in",
-          color: ["rgba(18, 21, 96, 0)", "rgba(18, 21, 96, 0.17)"],
+          color: ["rgba(18, 21, 96, 0)", "rgba(18, 21, 96, 0.10)"],
         },
       },
       directions: {
